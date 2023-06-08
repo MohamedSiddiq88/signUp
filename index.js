cd32dfd384fa12df9ed13e60ee3e33d4fcdf8581
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-08 16:33:38 +0530

    new

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app=express();
 
 //middleware
 app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
